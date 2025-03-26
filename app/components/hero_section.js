c666acdcf190190ef6b8d25134439d7ce4a3d8a8
sinthujank-Navicode <sinthujank.navicode@gmail.com>
2025-03-26 18:17:47 +0530

    change color

--- app/components/hero_section.js
+++ app/components/hero_section.js
@@ -1,7 +1,7 @@
 // app/components/Section1.js
 export default function Section1() {
     return (
-      <section className="h-screen bg-gray-500  flex items-center justify-center">
+      <section className="h-screen bg-white flex items-center justify-center">
         <h1 className="text-white text-5xl"> Hero Section </h1>
       </section>
     );
