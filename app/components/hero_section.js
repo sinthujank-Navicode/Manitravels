acef61d02db046b05de86f318991b4e66b486003
sinthujank-Navicode <sinthujank.navicode@gmail.com>
2025-03-26 18:11:19 +0530

    Build Navbar

---
+++ app/components/hero_section.js
@@ -0,0 +1,9 @@
+// app/components/Section1.js
+export default function Section1() {
+    return (
+      <section className="h-screen bg-gray-500  flex items-center justify-center">
+        <h1 className="text-white text-5xl"> Hero Section </h1>
+      </section>
+    );
+  }
+  
